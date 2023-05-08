059411a7c99ea1ac567e4bd0534b39d783822678
Jade <rockbody03182@gmail.com>
2023-05-08 21:56:57 +0900

    [fix] 모달창 종목 Input 포커스 할 시 툴팁 활성화 수정
    종목 Input 포커스 할 시 툴팁 활성화 수정. 이제 정상적인 툴팁 활성화 가능
    
    Issue 없음

--- src/Components/AddEvent.js
+++ src/Components/AddEvent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import Reac, { useState } from "react";
 import {
   Button,
   useDisclosure,
@@ -8,27 +8,59 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,
   Text,
   Checkbox,
   CheckboxGroup,
   Stack,
   Flex,
+  Grid,
+  GridItem,
+  Input,
+  InputGroup,
+  useColorMode,
+  Tooltip
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { AddIcon } from "@chakra-ui/icons";
 
 export default function AddEvent() {
+  
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [unitPriceValue, setUnitPriceValue] = useState(""); // 보유수량
+
   // 종목 추가 Modal Component
   return (
     <>
-      <FontAwesomeIcon
-        className="fa-spin"
-        onClick={onOpen}
-        icon={faGear}
-        size="xl"
-      />
+      <Tooltip
+        hasArrow
+        label="종목 추가"
+        aria-label="Add Event"
+        placement="top"
+        bg={colorMode === "light" ? "gray.300" : "gray.600"}
+        color={colorMode === "light" ? "gray.600" : "gray.200"}
+        borderRadius="5px"
+        p="5px 10px"
+        fontWeight="500"
+      >
+      <Button
+        bg={colorMode === "light" ? "#fff" : "1B1B1E"}
+        color={colorMode === "light" ? "gray.500" : "gray.300"}
+        _hover={
+          colorMode === "light"
+            ? { bg: "#fff", color: "gray.700" }
+            : { bg: "#1B1B1E", color: "gray.50" }
+        }
+        >
+        <FontAwesomeIcon
+          className="fa-spin"
+          onClick={onOpen}
+          icon={faGear}
+          size="xl"
+        />
+        </Button>
+      </Tooltip>
       <Modal
         isOpen={isOpen}
         onClose={onClose}
@@ -41,15 +73,41 @@ export default function AddEvent() {
           <ModalHeader>
             <Flex alignItems="center"></Flex>
           </ModalHeader>
-          {/* <ModalCloseButton /> */}
           <ModalBody>
             <div className="flex">
               <Stack spacing={5} direction="row">
-                <Checkbox colorScheme="red" defaultChecked>
-                  Checkbox
-                </Checkbox>
+                <CheckboxGroup>
+                  <Checkbox colorScheme="red" size="lg"/>
+                </CheckboxGroup>
+                <GridItem rowSpan={1} colSpan={2}>
+                  {/* 종목 Input */}
+                  <InputGroup>
+                    <Input
+                      className={
+                        colorMode === "light"
+                          ? "calculator-light-input"
+                          : "calculator-dark-input"
+                      }
+                      bg={colorMode === "light" ? "#fff" : "#303134"}
+                      color={colorMode === "light" ? "gray.500" : "gray.300"}
+                      type="text"
+                      placeholder="종목"
+                      // value={unitPriceValue}
+                      // onChange={}
+                      borderColor="#303134"
+                      _placeholder={{ opacity: 0.6, color: "#fff" }}
+                    />
+                  </InputGroup>
+                </GridItem>
               </Stack>
             </div>
+            <Flex alignItems="center" justify="center" marginTop="1rem">
+              <Stack direction='row' spacing={4} align='center'>
+                <Button colorScheme='teal' variant='ghost'>
+                  <AddIcon boxSize={4} />
+                </Button>
+              </Stack>
+            </Flex>
           </ModalBody>
           <ModalFooter justifyContent="center">
             <Button colorScheme="red" mr={3} onClick={onClose}>
--- src/Components/SelectEvent.js
+++ src/Components/SelectEvent.js
@@ -3,13 +3,9 @@ import {
   Select,
   Flex,
   Box,
-  Button,
   useColorMode,
-  Tooltip,
   useDisclosure,
 } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
 import AddEvent from "./AddEvent"; // 종목 추가 Modal Component
 
 import "../css/selectevent.css";
@@ -37,29 +33,7 @@ export default function Selected() {
             />
           </div>
         </Box>
-        <Tooltip
-          hasArrow
-          label="종목 추가"
-          aria-label="Add Event"
-          placement="top"
-          bg={colorMode === "light" ? "gray.300" : "gray.600"}
-          color={colorMode === "light" ? "gray.600" : "gray.200"}
-          borderRadius="5px"
-          p="5px 10px"
-          fontWeight="500"
-        >
-          <Button
-            bg={colorMode === "light" ? "#fff" : "1B1B1E"}
-            color={colorMode === "light" ? "gray.500" : "gray.300"}
-            _hover={
-              colorMode === "light"
-                ? { bg: "#fff", color: "gray.700" }
-                : { bg: "#1B1B1E", color: "gray.50" }
-            }
-          >
-            <AddEvent />
-          </Button>
-        </Tooltip>
+        <AddEvent />
       </Flex>
     </>
   );
